aa0b60b78eaae659953b6d475b95ee7d07def132
Mohamed Mahmoud <mohamed@makeitsimple.info>
2019-09-30 21:37:08 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PrototypeComponent} from './prototype/prototype.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {component: PrototypeComponent, path: 'prototype'}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
